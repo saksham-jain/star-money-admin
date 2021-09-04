d49d8b502e39d84c31f18704917fa590fad05a2d
Saksham Jain <sajain@gammastack.com>
2021-09-04 11:29:22 +0530

    Adds visitor tracking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_161135) do
+ActiveRecord::Schema.define(version: 2021_09_03_142949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 2021_08_21_161135) do
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_admin_users_on_role_id"
+  end
+
+  create_table "analytics", force: :cascade do |t|
+    t.integer "unique_visits", default: 0
+    t.integer "total_visits", default: 0
   end
 
   create_table "articles", force: :cascade do |t|
